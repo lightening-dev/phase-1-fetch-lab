bb901b85a4ad6f67cc0027f52f0eacc8c970f2e5
Lightening Gibson <lighteningibson@gmail.com>
2022-10-04 10:07:14 -0500

    commit tree 2fe49dbd0c5279db01534e72fabfdc5cc51cd9e8

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
   
-}
+  return fetch('https://anapioficeandfire.com/api/books').then(resposne =>
+    resposne.json()).then(data => renderBooks(data))
+  
+  }
+
+   
+
+    
+  
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
